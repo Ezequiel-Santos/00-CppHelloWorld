e17493f6af8c2388e52500995898cbc7fd40469b
Ezequiel Santos <103851433+Ezequiel-Santos@users.noreply.github.com>
2022-06-02 19:15:36 -0300

    Update Pruebas de tipos de datos.cpp

--- DD-EjemploTipos/Pruebas de tipos de datos.cpp
+++ DD-EjemploTipos/Pruebas de tipos de datos.cpp
@@ -17,23 +17,25 @@ int main() {
     assert (2.0==1.0+1.0);
     assert (1.0==0.1*10.0);
     assert (1.0!=0.1+0.1+0.1+0.1+0.1+0.1+0.1+0.1+0.1+0.1);
+    // long
+    assert (7000 < 7500)
+    assert (7000 + 1000 = 8000)
     //integer
     assert (-1-2==-3);
     assert (-1+1==0);
     assert (-1*2>0);
     assert (-6/-3>0);
     //unsigned
-    assert (10==1+1+1+1+1+1+1+1+1+1);
-    assert (10/2==5);
-    assert (5*5==25);
+    assert (10u==1u+1u+1u+1u+1u+1u+1u+1u+1u+1u);
+    assert (10u/2u==5u);
+    assert (5u*5u==25u);
     //character
-    assert (65+1==66);
-    assert (10-1==9);
-    assert (10*10+10/10==101);
+    assert ("c"-"a"=2)
     assert ("a"+1=="b");
     assert ("b"-1=="a");
     assert ("Hola mundo" == "Hola mundn"+1);
     //string
     assert ("Hola mundo" != "Adios mundo");
     assert ("Hola mundo" == "Hola mundo");
+    assert (12 == "Las Palabras"s.length())
 }
